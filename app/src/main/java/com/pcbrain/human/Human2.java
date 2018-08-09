de236b5edd8e2cf65d92ba6df3db2f3f624744ba
chabinmi <chabin_mi69050219@yahoo.co.jp>
2018-08-09 17:15:13 +0900

    08/08 disunderstandable commit&push

--- app/src/main/java/com/pcbrain/human/Human2.java
+++
@@ -1,9 +0,0 @@
-import android.util.Log;
-
-class Human2 extends Animal implements Thinkable {
-
-    @Override
-    public void think() {
-        Log.d("javatest", "私は" + this.hobby + "について考える。");
-    }
-}
